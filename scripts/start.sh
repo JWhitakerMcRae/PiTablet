ebfec678789cd8f69a9d45591488375c7e52e20a
J. Whitaker McRae <jwhitakermcrae@Js-Mac-mini.home>
2018-03-01 19:49:49 -0500

    Adding lcd_rotate code back in for now

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 echo "Running start.sh ..."
 # ensure lcd_rotate=2 is set in boot partition config (leave file cookie once done to avoid mounting to check each time!)
-#checkfile="/home/pitablet/screen_orientation_fixed"
-#if [ ! -f "$checkfile" ]
-#then
-#	echo "Fixing screen orientation ..."
-#	mkdir /boot_tmp
-#	mount /dev/mmcblk0p1 /boot_tmp
-#	sed -i "s/#display_rotate=0/lcd_rotate=2/g" /boot_tmp/config.txt
-#	umount /boot_tmp
-#	rm -rf /boot_tmp
-#	touch $checkfile
-#	echo "*** PLEASE PERFORM HARD REBOOT OF DEVICE ***"
-#fi
+checkfile="/home/pitablet/screen_orientation_fixed"
+if [ ! -f "$checkfile" ]
+then
+	echo "Fixing screen orientation ..."
+	mkdir /boot_tmp
+	mount /dev/mmcblk0p1 /boot_tmp
+	sed -i "s/#display_rotate=0/lcd_rotate=2/g" /boot_tmp/config.txt
+	umount /boot_tmp
+	rm -rf /boot_tmp
+	touch $checkfile
+	echo "*** PLEASE PERFORM HARD REBOOT OF DEVICE ***"
+fi
 echo "Done!"
