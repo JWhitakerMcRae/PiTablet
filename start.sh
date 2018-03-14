39b00e5b01f217bf233f241fd9133bb22da06509
J. Whitaker McRae <jwhitakermcrae@Js-Mac-mini.home>
2018-03-13 20:15:41 -0400

    Re-adding force orientation fix on boot

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 echo "Running start.sh ..."
+# Ensure lcd_rotate=2 is NOT set in boot partition config (leave file cookie once done to avoid mounting to check each time!)
+mkdir /boot_tmp
+mount /dev/mmcblk0p1 /boot_tmp
+sed -i "s/lcd_rotate=2/#display_rotate=0/g" /boot_tmp/config.txt
+umount /boot_tmp
+rm -rf /boot_tmp
+# Start app
 #FIXME python3 /app/main.py
-echo "Done!"
